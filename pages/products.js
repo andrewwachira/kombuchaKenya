239ef025676f288504a13a96137f4e7960d01d07
Andrew Wachira <andrewwachira1@gmail.com>
2022-05-21 18:25:38 +0300

    adding user-agent header on api call

--- pages/products.js
+++ pages/products.js
@@ -10,7 +10,10 @@ import { server } from "../config/index.js";
 
 
 export async function getStaticProps(){
-	const res = await fetch(`${server}/api/products`);
+	const res = await fetch(`${server}/api/products`,{
+		headers: {
+		  'User-Agent': 'ANYTHING_WILL_WORK_HERE'
+		}});
 	const products =  await res.json();
 	return {
 		props : {
