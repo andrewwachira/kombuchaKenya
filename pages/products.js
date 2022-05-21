501f87fccf9eed8778d9a85a8934bbf3a7d05a23
andrewwachira <50460639+andrewwachira@users.noreply.github.com>
2022-05-21 16:06:29 +0300

    Update products.js
    fixing api url to absolute

--- pages/products.js
+++ pages/products.js
@@ -8,7 +8,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export async function getStaticProps(){
-	const res = await fetch("www.kombuchakenya.co.ke/api/products");
+	const res = await fetch("https://www.kombuchakenya.co.ke/api/products");
 	const products =  await res.json();
 	return {
 		props : {
